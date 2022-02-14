797c8a1a47897fe881885b664e1cb257e43063a6
Peerchemist <dx.1178@gmail.com>
2022-02-14 15:41:41 +0100

    adding db conf

--- app/conf.py
+++ app/conf.py
@@ -5,11 +5,17 @@ if environ.get('APP_ENV') == 'docker':
     node_api_key = environ.get('NODE_API_KEY', 'nodeapikey')
     node_ip = environ.get('NODEIP', 'vsystems')
     block_time = int(environ.get('BLOCK_TIME', '4'))
+    db_user = environ.get('DB_USER')
+    db_pass = environ.get('DB_PASS')
+    db_ip = environ.get('DB_IP')
 
 else:
     node_api_key = "5&!aJ#gyu2i#"
     node_ip = "gabija.vos.systems"
     block_time = 4
+    db_user = "postgres"
+    db_pass = "postgres"
+    db_ip = "0.0.0.0"
 
 
 with open("conf.yaml", "r") as f:
