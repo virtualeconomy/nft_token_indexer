92769710a6a7904136888c59d9a785d8b82285e1
Peerchemist <dx.1178@gmail.com>
2022-02-15 16:18:55 +0100

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -2,14 +2,23 @@ import uvicorn
 from fastapi import FastAPI
 
 from sqlalchemy import select
+
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi_asyncalchemy.models import *
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 @app.get("/")
 async def hello_world():
     return "hello_world"
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=['Access-Control-Allow-Origin'],
+)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=1111, host='127.0.0.1')
