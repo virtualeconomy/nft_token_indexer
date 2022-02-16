74d697ea5e7ba66f42cdcdf2b851bb4a2c9242b9
Peerchemist <dx.1178@gmail.com>
2022-02-16 18:25:31 +0100

    expand on using asyncpg

--- app/base.py
+++ app/base.py
@@ -2,6 +2,7 @@ import asyncio
 import asyncpg
 
 import conf
+from log import logger
 
 
 async def init_table(ctrt_id: str):
@@ -21,4 +22,35 @@ async def init_table(ctrt_id: str):
 
     await conn.close()
 
+
+async def test_insert():
+
+    conn = await asyncpg.connect(user=conf.db_user, password=conf.db_pass,
+                             database=conf.db, host=conf.db_ip)
+    
+    await conn.execute(
+        """INSERT INTO cf2pag83harcsmp9s9m2xegajupqwkfarxr VALUES('ATytsw58Q1PBUcduYnA6iwT8S8jHZcLTz5i', 1, 1);"""
+    )
+
+    await conn.close()
+
+
+async def test_read():
+
+    conn = await asyncpg.connect(user=conf.db_user, password=conf.db_pass,
+                            database=conf.db, host=conf.db_ip)
+
+    q = await conn.fetch(
+        """SELECT token_idx
+           FROM cf2pag83harcsmp9s9m2xegajupqwkfarxr
+           WHERE user_addr = 'ATytsw58Q1PBUcduYnA6iwT8S8jHZcLTz5i';
+        """)
+
+    logger.info(q[0]['token_idx'])
+
+    await conn.close()
+
+    return q
+
 #asyncio.run(init_table())
+asyncio.run(test_read())
