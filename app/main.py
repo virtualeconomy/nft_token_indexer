63dae4d9a64d81f10be03c15590e45e3e101019e
Peerchemist <dx.1178@gmail.com>
2022-02-17 19:18:57 +0100

    fastapi -> starlite

--- app/main.py
+++ app/main.py
@@ -1,36 +1,42 @@
 import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import asyncpg
+
+from typing import List
+from starlette.status import HTTP_202_ACCEPTED
+from starlite import Starlite, CORSConfig, MediaType, get
 
 import conf
+from log import logger
 
-app = FastAPI()
-
-@app.get("/")
-async def hello_world():
-    return "hello_world"
-
-@app.get("/associatedtokens/{contract_id}/{address}")
-async def associatedtokens(contract_id: str, address: str):
-
-    conn = await asyncpg.connect(user=conf.db_user, password=conf.db_pass,
-                                 database=conf.db, host=conf.db_ip)
-    
-    query = await conn.execute(f'''
-        SELECT token_idx
-        FROM {contract_id}
-        WHERE user_addr = {address};
-    ''')
-
-    return [i['token_idx'] for i in query]
+cors_config = CORSConfig(allow_origins=["*"])
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_methods=["*"],
-    allow_headers=['Access-Control-Allow-Origin'],
+@get(path="/ping", media_type=MediaType.TEXT)
+async def ping() -> str:
+    """health check"""
+    return "pong"
+
+
+@get(
+    path="/associatedtokens/{contract_id:str}/{address:str}",
+    status_code=HTTP_202_ACCEPTED,
+    media_type=MediaType.JSON,
 )
+async def associatedtokens(contract_id: str, address: str) -> int:
+    """query db for which token id's are associated with some address"""
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", port=1111, host='127.0.0.1')
+    conn = await asyncpg.connect(
+        user=conf.db_user, password=conf.db_pass, database=conf.db, host=conf.db_ip
+    )
+
+    query = await conn.fetchrow(
+        f"""
+        SELECT *
+        FROM "{contract_id}"
+        WHERE user_addr = '{address}';
+    """
+    )
+
+    return query['token_idx']
+
+app = Starlite(route_handlers=[ping, associatedtokens], cors_config=cors_config)
