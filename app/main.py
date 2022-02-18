124d325067b1343451165b43b2efddcf2c20e1a4
Peerchemist <dx.1178@gmail.com>
2022-02-18 09:04:51 +0100

    return proper tokenID

--- app/main.py
+++ app/main.py
@@ -2,13 +2,20 @@ import uvicorn
 import asyncpg
 
 from typing import List
+from pydantic import BaseModel
 from starlette.status import HTTP_202_ACCEPTED
 from starlite import Starlite, CORSConfig, MediaType, get
 
 import conf
 from log import logger
 
+import py_v_sdk as pv
+
 cors_config = CORSConfig(allow_origins=["*"])
+
+
+class TokenID(BaseModel):
+    tokenID: str
 
 
 @get(path="/ping", media_type=MediaType.TEXT)
@@ -22,7 +29,7 @@ async def ping() -> str:
     status_code=HTTP_202_ACCEPTED,
     media_type=MediaType.JSON,
 )
-async def associatedtokens(contract_id: str, address: str) -> List:
+async def associatedtokens(contract_id: str, address: str) -> List[TokenID]:
     """query db for which token id's are associated with some address"""
 
     conn = await asyncpg.connect(
@@ -31,13 +38,15 @@ async def associatedtokens(contract_id: str, address: str) -> List:
 
     query = await conn.fetch(
         f"""
-        SELECT *
+        SELECT token_idx
         FROM "{contract_id}"
         WHERE user_addr = '{address}';
     """
     )
 
-    return [r['token_idx'] for r in query]
+    await conn.close()
+
+    return [pv.Ctrt.get_tok_id(contract_id, dict(r)['token_idx']) for r in query]
 
 
 app = Starlite(route_handlers=[ping, associatedtokens], cors_config=cors_config)
